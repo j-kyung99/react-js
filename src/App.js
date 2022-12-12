68f58d6b57b4258860773efefac82bc7e814edd9
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-12-12 21:06:40 +0900

    #7.2 & Challenge

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [result, setResult] = useState(0);
+  const [coinSymbol, setCoinSymbol] = useState("");
+  const [coinMoney, setCoinMoney] = useState(0);
+  const onChangeInput = (event) => setMoney(event.target.value);
+  const onChangeSelect = (event) => {
+    const price = coins[event.target.selectedIndex - 1].quotes.USD.price;
+    const symbol = coins[event.target.selectedIndex - 1].symbol;
+    setCoinSymbol(symbol);
+    setCoinMoney(parseInt(price));
   };
-  const onDelete = (event) => {
-    const li = event.target.parentElement;
-    li.remove();
-  };
+  useEffect(() => {
+    setResult(Math.round((money / coinMoney) * 100) / 100);
+  }, [money, coinMoney]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={onDelete}>❌</button>
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <form>
+          <input type="number" value={money} onChange={onChangeInput} />
+          <hr />
+          <select onChange={onChangeSelect}>
+            <option>Select coin !</option>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        </form>
+      )}
+      <h2>
+        You have {money} dollars & You can buy {result} {coinSymbol}
+      </h2>
     </div>
   );
 }
