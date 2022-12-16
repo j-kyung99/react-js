43bcdde2db8ad32cc8ebd40da6e06304a88befac
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-12-16 16:57:34 +0900

    #7.5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
